58f5d5731aa320e683ed3069406fc7d2962640f7
phantom99a1 <viendanho99a1@gmail.com>
2025-01-16 22:57:21 +0700

    Add class entity

---
+++ Dapper/Domain/Entities/Certificate.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    [Table("Certificate")]
+    public class Certificate
+    {
+        [Key]
+        public int CertId { get; set; }
+
+        [Column("StudentId")]
+        public int StudentId { get; set; }
+
+        [Column("CertName")]
+        public string CertName { get; set; } = string.Empty;
+
+        [Column("CertCode")]
+        public string CertCode { get; set; } = string.Empty;
+    }
+}
--- Dapper/Domain/Entities/Student.cs
+++ Dapper/Domain/Entities/Student.cs
@@ -9,13 +9,35 @@ namespace Domain.Entities
         [Key]
         [Column("Id")]
         public int Id { get; set; }
+
         [Column("FirstName")]
         public string FirstName { get; set; } = string.Empty;
+
         [Column("LastName")]
         public string LastName { get; set; } = string.Empty;
+
         [Column("EmailAddress")]
         public string EmailAddress { get; set; } = string.Empty;
+
         [Column("Major")]
         public string Major { get; set; } = string.Empty;
+
+        [Column("Deleted")]
+        public bool Deleted { get; set; }
+
+        [Column("Version")]
+        public int Version { get; set; }
+
+        [Column("CreatedDate")]
+        public DateTime? CreatedDate { get; set; }
+
+        [Column("CreatedUser")]
+        public string CreatedUser { get; set; } = string.Empty;
+
+        [Column("UpdatedDate")]
+        public DateTime? UpdatedDate { get; set; }
+
+        [Column("UpdatedUser")]
+        public string UpdatedUser { get; set; } = string.Empty;
     }
 }
